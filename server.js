c32b2c4dc832087b45698d008c2f645ec66b70fd
Weronika <weronika.k11@gmail.com>
2020-12-10 21:20:47 +0100

    commit poprawa server.js

--- server.js
+++ server.js
@@ -3,13 +3,17 @@ var express = require("express")
 var app = express()
 var PORT = process.env.PORT || 3000; // bardzo istotna linijka - port zostaje przydzielony przez Heroku
 
-app.get("/", function (req, res) {
-    res.send("<h1>first app on heroku!</h1>")
-})
+// app.get("/", function (req, res) {
+//     res.send("<h1>first app on heroku!</h1>")
+// })
 
-app.listen(PORT, function () {
-    console.log("start serwera na porcie " + PORT)
-})
+// app.listen(PORT, function () {
+//     console.log("start serwera na porcie " + PORT)
+// })
+
+// var express = require("express")
+// var app = express()
+// const PORT = 3000;
 
 var path = require("path")
 
